bd8e323ed132a78ed5f1d1bde03298273e6f6c24
RAM <ramdadi2004@gmail.com>
2026-04-07 16:29:24 +0530

    Refactor salary prediction code structure

--- sl.py
+++ sl.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-# Title
 st.title("Salary Prediction System")
 
-# Dataset
 data = {
     "experience": [1, 2, 3, 4, 5, 3, 3],
     "skills":     [2, 4, 5, 7, 8, 9, 1],
@@ -14,18 +12,15 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Train Model
 X = df[["experience", "skills"]]
 y = df["salary"]
 
 model = LinearRegression()
 model.fit(X, y)
 
-# User Input
 exp = st.slider("Years of Experience", 0, 10, 1)
 skills = st.slider("Skill Level (1-10)", 1, 10, 5)
 
-# Prediction Button
 if st.button("Predict Salary"):
     input_data = pd.DataFrame([[exp, skills]], columns=["experience", "skills"])
     prediction = model.predict(input_data)
